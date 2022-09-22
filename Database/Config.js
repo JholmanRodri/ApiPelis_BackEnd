c84e62c36c3e46508fa0d43cf9db44b19de030e9
Jholman <jholmans1234@gmail.com>
2022-09-21 22:37:39 -0500

    a

--- Database/Config.js
+++ Database/Config.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 const bdconectar= async()=>{
     try {
-        await mongoose.connect(process.env.MONGOBD_CNX);
+        mongoose.connect(process.env.MONGOBD_CNX);
         console.log("Base de datos online");
     } catch (error) {
         throw new Error("Error al iniciar la base de datos")
